be08c6feb99463623fd2ebcf93e27035a9017aa1
picarro-sbrar <sbrar@picarro.com>
2016-09-22 14:42:04 -0700

    initial commit

---
+++ JavaPractice/src/Lamp.java
@@ -0,0 +1,15 @@
+
+public class Lamp {
+	
+	double diameter;
+	boolean lighted;
+	int wattage;
+	
+    public double getDiamter(){
+    	return diameter;
+    }
+    
+    public boolean isLighted(){
+    	return lighted;
+    }
+}
